0404fca9908216e89337ff8e7eba985949482c24
Allen Shaw <allen@JoineryHQ.com>
2025-01-19 15:34:59 -0600

    Add proof-of-concept data.

---
+++ CRM/Stepw/Fixme/Data.php
@@ -0,0 +1,22 @@
+<?php
+use CRM_Stepw_ExtensionUtil as E;
+
+class CRM_Stepw_Fixme_Data {
+  public static function getSampleData() {
+    return [
+      '1' => [
+        '1' =>  [
+          'afform_name' => 'afformQuickAddIndividual',
+          'url' => 'http://plana.l/?page_id=5&civiwp=CiviCRM&q=civicrm%2Fquick-add%2FIndividual',
+        ],
+        '2' => [
+          'afform_name' => 'afformTEST',
+          'url' => 'http://plana.l/wp-admin/admin.php?page=CiviCRM&q=civicrm%2Fform-test',
+        ],
+      ],
+    ];
+  }
+}
+
+  
+
