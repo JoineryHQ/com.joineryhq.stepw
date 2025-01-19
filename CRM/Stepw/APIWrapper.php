8e91d1b6f9f9167f4229d83819751aa2f4a3e60b
Allen Shaw <allen@JoineryHQ.com>
2025-01-19 15:38:42 -0600

    APIWrapper: use http_referrer to track state (proof-of-concept).

--- CRM/Stepw/APIWrapper.php
+++ CRM/Stepw/APIWrapper.php
@@ -8,20 +8,35 @@
 class CRM_Stepw_APIWrapper {
 
   public static function RESPOND($event) {
-    $request = $event->getApiRequestSig();
-    $apiRequest = $event->getApiRequest();
-    $result = $event->getResponse();
+    $requestSignature = $event->getApiRequestSig();
 
-    if ($request == "4.afform.get") {
-      $a = $result[0];
-      $a = 1;
+    // fixme: only act if we're sure we're in a Stepwise workflow (examine $_GET, probably?)
+    // fixme: if we're going to pass state via $_GET, we MUST have some way (a hash maybe) to ensure query params like 'sw' as 'ss' are really ours (not coliding with some other extension)
+    
+    if ($requestSignature == "4.afform.get") {
+      $responseValues = $event->getResponse();
 
-      if (!empty($result[0]['redirect'])) {
-        $result[0]['redirect'] = 'http://example.com';
-        $session = $_SESSION['CiviCRM'];
-        $get = $_GET;
-        $event->setResponse($result);
+      $configData = CRM_Stepw_Utils::getWorkflowConfig();
+      
+      // Track worfklowid and stepid via referrer, because the xhr request that
+      // gets us to this point is not easily overloaded in order to put sw and ss 
+      // into the post/get values. 
+      // fixme: should add some kind of hashing as well, to make this more trustworty.
+      parse_str(parse_url($_SERVER['HTTP_REFERER'], PHP_URL_QUERY), $queryParams);
+      $workflowId = $queryParams['sw'];
+      $stepId = $queryParams['ss'];
+      
+      // determine redirect.
+      // fixme: need a solid way to handle the final page.
+      $workflow = $configData[$workflowId];
+      $nextStepId = $stepId + 1;
+      // fixme: refactor this into a util function, once we're decided on using query params to track state.
+      $redirect = CRM_Utils_System::url('/civicrm/stepwise', ['sw' => $workflowId, 'ss' => $nextStepId], TRUE, NULL, FALSE);
+      
+      foreach ($responseValues as &$responseValue) {
+        $responseValue['redirect'] = urldecode($redirect);
       }
+      $event->setResponse($responseValues);
     }
   }
 
