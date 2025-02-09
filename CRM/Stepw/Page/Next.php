9a59b6b2757f3955beefd5d81e5dbfd79c2e9166
Allen Shaw <allen@JoineryHQ.com>
2025-02-09 17:39:06 -0600

    "Next": refactoring, pretty good functionality for state handling.

--- CRM/Stepw/Page/Next.php
+++ CRM/Stepw/Page/Next.php
@@ -3,66 +3,62 @@ use CRM_Stepw_ExtensionUtil as E;
 
 class CRM_Stepw_Page_Next extends CRM_Core_Page {
 
+  private $workflowInstancePublicId;
+  private $doneStepPublicId;
+  private $workflowInstance;
+  
   public function run() {
 
-    // fixme: this is very much WIP.
-    $workflowInstancePublicId = CRM_Stepw_Utils_Userparams::getUrlQueryParams(CRM_Stepw_Utils_Userparams::QP_WORKFLOW_INSTANCE_ID);
-    $doneStepPublicId = CRM_Stepw_Utils_Userparams::getUrlQueryParams(CRM_Stepw_Utils_Userparams::QP_DONE_STEP_ID);
-    if (!empty($doneStepPublicId)) {
-      $isValid = CRM_Stepw_State::singleton()->validateWorkflowInstanceStep($doneStepPublicId, 'open');
-      if (!$isValid) {
-//        CRM_Stepw_Utils_General::redirectToInvalid();
-      }
-      
-      $workflow = CRM_Stepw_State::singleton()->getWorkflowInstance($workflowInstancePublicId);
-      $workflow->closeStep($doneStepPublicId);
+    $this->workflowInstancePublicId = CRM_Stepw_Utils_Userparams::getUrlQueryParams(CRM_Stepw_Utils_Userparams::QP_WORKFLOW_INSTANCE_ID);
+    $this->workflowInstance = CRM_Stepw_State::singleton()->getWorkflowInstance($this->workflowInstancePublicId);
+    $this->doneStepPublicId = CRM_Stepw_Utils_Userparams::getUrlQueryParams(CRM_Stepw_Utils_Userparams::QP_DONE_STEP_ID);    
+        
+    if (!$this->validate()) {
+      CRM_Stepw_Utils_General::redirectToInvalid();
+    }
+
+    // fixme: if done step is video page, denote 'video watched for this step' in the workflow instance (this property won't be archived even if step is archived);
+
+    if (!empty($this->doneStepPublicId)) {
+      // ensure closure of done step (if given);
+      $this->workflowInstance->closeStep($this->doneStepPublicId);
     }
     
-    $workflowInstance = CRM_Stepw_State::singleton()->getWorkflowInstance($workflowInstancePublicId);
-    $workflowId = $workflowInstance->getVar('workflowId');
-    $workflow = CRM_Stepw_Utils_WorkflowData::getWorkflowConfigById($workflowId);
-
-    $fixmeNextStepKey = 2;
-
-    $step = $workflow[$fixmeNextStepKey];
+    // fixme: determine last completed step (based only on closed steps); 
+    // fixme: if last step was afform, apply any configured post-save validation for step;
+    // fixme: if any post-save validation fails: redirect to post-save validation failure message;
     
+    // determine next step (based only on closed steps);
+    $workflowInstanceNextStep = $this->workflowInstance->getNextStep();
+
     // Open next step in workflowInstance.
-    $stepPublicId = $workflowInstance->openStep($fixmeNextStepKey);
+    $stepPublicId = $this->workflowInstance->openStep($workflowInstanceNextStep['stepId']);
 
     // Append parameters to step url and redirect thence.
     $params = [
+      'i' => $this->workflowInstance->getVar('publicId'),
       's' => $stepPublicId,
-      'i' => $workflowInstance->getVar('publicId'),
+      // fixme: we must append afform params for existing contact_id if one was created
+      // earlier in the workflow.
     ];
-    $redirect = CRM_Stepw_Utils_Userparams::appendParamsToUrl($params, $step['url']);
+    $redirect = CRM_Stepw_Utils_Userparams::appendParamsToUrl($params, $workflowInstanceNextStep['url']);
+
     CRM_Utils_System::redirect($redirect);
 
-    
-    die(__METHOD__);
-    $workflowId = CRM_Utils_Request::retrieve('sw', 'Int');
-    $fixmeNextStepKey = CRM_Utils_Request::retrieve('ss', 'Int');
-    $workflow = CRM_Stepw_Utils_WorkflowData::getWorkflowConfigById($workflowId);
-    // fixme: if no stepid, we should start at step 1, but also need a mechanism to ensure previous steps have been completed -- i.e., no bookmarking 'step 2' and starting there.
-    if (empty($fixmeNextStepKey)) {
-      $fixmeNextStepKey = 1;
-    }
-      
-    $step = $workflow[$fixmeNextStepKey] ?? NULL;
-    if (empty($workflow)) {
-      die('fixme: workflow not found; should return 404');
-    }
-    if (empty($step)) {
-      die('fixme: step not found in this workflow; should return 404');
-    }
-    
-    $params = [
-      'sw' => $workflowId,
-      'ss' => $fixmeNextStepKey,
-    ];
-    // fixme: this won't be the correct redirect
-    $redirect = CRM_Stepw_Utils_Userparams::appendParamsToUrl($params, $step['url']);
-    die('fixme: probably need to fix this redirect url: '. $redirect);
-    CRM_Utils_System::redirect($redirect);
+
   }
-
+  
+  private function validate() {
+    $isValid = FALSE;
+    
+    if (!empty($this->workflowInstance)) {
+      if (
+        empty($this->doneStepPublicId)
+        || CRM_Stepw_State::singleton()->validateWorkflowInstanceStep($this->doneStepPublicId)
+      ) {
+        $isValid = TRUE;
+      }
+    }
+    return $isValid;
+  }
 }
