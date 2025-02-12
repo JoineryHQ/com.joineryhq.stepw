6566cd17ad76e9e17acd01d57e7ec7aaeb00fde1
Allen Shaw <allen@JoineryHQ.com>
2025-02-11 18:28:53 -0600

    Fixme_Data: omit array keys in steps.

--- CRM/Stepw/Fixme/Data.php
+++ CRM/Stepw/Fixme/Data.php
@@ -7,25 +7,25 @@ class CRM_Stepw_Fixme_Data {
       '1' => [
         'steps' => [
           // TODO: when loading real data, ensure these are sorted in step order.
-          '1' =>  [
+          [
             'type' => 'post',
             'url' => 'http://plana.l/example-intro-page/',
           ],
-          '2' =>  [
+          [
             'type' => 'afform',
             'url' => 'http://plana.l/civicrm/test-form-start/',
             'afform_name' => 'afformTESTFormStart',
           ],
-          '3' => [
+          [
             'type' => 'post',
             'url' => 'http://plana.l/example-video-page/',
           ],
-          '4' =>  [
+          [
             'type' => 'afform',
             'url' => 'http://plana.l/civicrm/test-form-2-activity/',
             'afform_name' => 'afformTestForm2Activity1',
           ],
-          '5' => [
+          [
             'type' => 'post',
             'url' => 'http://plana.l/example-final-page/',
           ],
