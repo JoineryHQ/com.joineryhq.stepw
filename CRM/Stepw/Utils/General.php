e52786c4b798f614bcdef855879cd315202e056f
Allen Shaw <allen@JoineryHQ.com>
2025-02-24 10:55:53 -0600

    General::redirectToInvalid: add searchable 'error-id' to logged messages, and display to user.

--- CRM/Stepw/Utils/General.php
+++ CRM/Stepw/Utils/General.php
@@ -20,17 +20,23 @@ class CRM_Stepw_Utils_General {
   }
 
   public static function redirectToInvalid(string $logMessage = '', string $publicMessage = '') {
-    // fixme3: if there's a logMessage, append a uniq log identifier both to the logMessage and publicMessage.
-    // This will allow users to report something that will be meaningful in debugging/log-inspection.
-    // This would be similar to what civicrm core does for ajax-context errors, as in
-    // line 159 of CRM/Api4/Page/AJAX.php:
-    // $error_id = rtrim(chunk_split(CRM_Utils_String::createRandom(12, CRM_Utils_String::ALPHANUMERIC), 4, '-'), '-')
-    //
-    if ($logMessage) {
-      \Civi::log()->debug(E::LONG_NAME .': '. $logMessage, $_REQUEST);
-    }
     if ($publicMessage) {
+      // Store any public message for display to the user.
       CRM_Stepw_State::singleton()->storeInvalidMessage($publicMessage);
+    }
+    if ($logMessage) {
+      // If there's a logMessage, append a uniq log identifier both to the logMessage and to a publicMessage.
+      // This will allow users to report something that will be meaningful in debugging/log-inspection.
+      // This would be similar to what civicrm core does for ajax-context errors, as in
+      // https://github.com/civicrm/civicrm-core/blob/5.81.0/CRM/Api4/Page/AJAX.php#L159
+      $error_id = rtrim(chunk_split(CRM_Utils_String::createRandom(12, CRM_Utils_String::ALPHANUMERIC), 4, '-'), '-');
+      $debugContext = [
+        'error_id' => $error_id,
+        '_REQUEST' => $_REQUEST,
+      ];
+      \Civi::log()->debug(E::LONG_NAME .': '. $logMessage, $debugContext);
+      // Store an additional informative message for dislpay to the user.
+      CRM_Stepw_State::singleton()->storeInvalidMessage(E::ts('When requesting help with this issue, please provide Log Reference Number: %1', ['1' => $error_id]));
     }
     $redirect = CRM_Utils_System::url('civicrm/stepwise/invalid', '', TRUE, NULL, FALSE);
     CRM_Utils_System::redirect($redirect);
