d717f2869319b80e5ffdee506f6f3c8137d1aeb9
Allen Shaw <allen@JoineryHQ.com>
2025-03-01 11:22:40 -0600

    Fixme_Data: use dynamic baseUrl for all urls (for easier transfer between e.g. dev and staging)

--- CRM/Stepw/Fixme/Data.php
+++ CRM/Stepw/Fixme/Data.php
@@ -3,6 +3,7 @@ use CRM_Stepw_ExtensionUtil as E;
 
 class CRM_Stepw_Fixme_Data {
   public static function getSampleData() {
+    $ufBaseUrl = CRM_Utils_File::addTrailingSlash(CIVICRM_UF_BASEURL);
     return [
       '1' => [
         'steps' => [
@@ -11,7 +12,7 @@ class CRM_Stepw_Fixme_Data {
             'options' => [
               [
                 'type' => 'url',
-                'url' => 'http://plana.l/example-intro-page/',
+                'url' => $ufBaseUrl . '/example-intro-page/',
                 // Option labels are the labels to display ON THIS OPTION/PAGE
                 // for each of the options (in order) that are available in the 
                 // next step.
@@ -27,7 +28,7 @@ class CRM_Stepw_Fixme_Data {
             'options' => [
               [
                 'type' => 'url',
-                'url' => 'http://plana.l/option-step-option-1/',
+                'url' => $ufBaseUrl . '/option-step-option-1/',
                 'requireOnpageEnforcer' => 1,
                 'optionLabels' => [
                   'Go to afform 1',
@@ -35,7 +36,7 @@ class CRM_Stepw_Fixme_Data {
               ],
               [
                 'type' => 'url',
-                'url' => 'http://plana.l/option-step-option-2/',
+                'url' => $ufBaseUrl . '/option-step-option-2/',
                 'requireOnpageEnforcer' => 1,
                 'optionLabels' => [
                   'Go to afform 1',
@@ -47,7 +48,7 @@ class CRM_Stepw_Fixme_Data {
             'options' => [
               [
                 'type' => 'afform',
-                'url' => 'http://plana.l/civicrm/test-form-start/',
+                'url' => $ufBaseUrl . '/civicrm/test-form-start/',
                 'afformName' => 'afformTESTFormStart',
                 // Because this step has at least one afform option (this one),
                 // the subsequent step MUST have only one option.
@@ -61,8 +62,8 @@ class CRM_Stepw_Fixme_Data {
             'options' => [
               [
                 'type' => 'afform',
-                'url' => 'http://plana.l/civicrm/test-form-2-activity/',
-                'afformName' => 'afformTestForm2Activity1',
+                'url' => $ufBaseUrl . '/civicrm/test-form-2-activity/',
+                'afformName' => 'afformTestForm2Activity',
                 // Because this step has at least one afform option (this one),
                 // the subsequent step MUST have only one option.
                 'optionLabels' => [
@@ -75,7 +76,7 @@ class CRM_Stepw_Fixme_Data {
             'options' => [
               [
                 'type' => 'url',
-                'url' => 'http://plana.l/example-final-page/',
+                'url' => $ufBaseUrl . '/example-final-page/',
                 'optionLabels' => [
                   // This page shouldn't have a button, and even if it does,
                   // the button shortcode should not display it, because it's last.
@@ -91,13 +92,13 @@ class CRM_Stepw_Fixme_Data {
 //          [],
 //          [
 //            'type' => 'afform',
-//            'url' => 'http://plana.l/civicrm/test-form-2-activity/',
+//            'url' => $ufBaseUrl . '/civicrm/test-form-2-activity/',
 //            'afformName' => 'afformTestForm2Activity1',
 //            'buttonLabel' => 'Submit and Next',
 //          ],
 //          [
 //            'type' => 'url',
-//            'url' => 'http://plana.l/example-intro-page/',
+//            'url' => $ufBaseUrl . '/example-intro-page/',
 //            'buttonLabel' => 'fixmeSplits: THIS WONT MATTER BECAUSE NEXT STEP IS "SPLIT"',
 //          ],
 //          [
@@ -109,19 +110,19 @@ class CRM_Stepw_Fixme_Data {
 //            'splits' => [
 //              [
 //                'type' => 'url',
-//                'url' => 'http://plana.l/option-step-option-1/',
+//                'url' => $ufBaseUrl . '/option-step-option-1/',
 //                'option_label' => 'Option 1',
 //                'buttonLabel' => 'Next (1)',
 //              ],
 //              [
 //                'type' => 'url',
-//                'url' => 'http://plana.l/option-step-option-2/',
+//                'url' => $ufBaseUrl . '/option-step-option-2/',
 //                'option_label' => 'Option 2',
 //                'buttonLabel' => 'Next (2)',
 //              ],
 //              [
 //                'type' => 'afform',
-//                'url' => 'http://plana.l/civicrm/test-form-2-activity/',
+//                'url' => $ufBaseUrl . '/civicrm/test-form-2-activity/',
 //                'afformName' => 'afformTestForm2Activity1',
 //                'buttonLabel' => 'Submit and Next (3)',
 //                'option_label' => 'Option 3 (afform)',
@@ -130,7 +131,7 @@ class CRM_Stepw_Fixme_Data {
 //          ],
 //          [
 //            'type' => 'afform',
-//            'url' => 'http://plana.l/civicrm/test-form-3-activity-2/',
+//            'url' => $ufBaseUrl . '/civicrm/test-form-3-activity-2/',
 //            'afformName' => 'afformTestForm3Activity2',
 //            'buttonLabel' => 'Submit and Next',
 //          ],
