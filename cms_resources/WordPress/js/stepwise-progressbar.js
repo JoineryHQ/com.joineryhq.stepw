b64384adc3bda9197f4912f67306fd91061025e8
Allen Shaw <allen@JoineryHQ.com>
2025-03-02 20:08:24 -0600

    stepwise-progressbar.js: handle placement of progressbar on civicrm forms (even lacking h1)

--- cms_resources/WordPress/js/stepwise-progressbar.js
+++ cms_resources/WordPress/js/stepwise-progressbar.js
@@ -9,8 +9,19 @@ if ((typeof CRM !== 'undefined') && '$' in CRM) {
 }
 jQuery(function ($) {
 
-  // Move progress bar to just before h1 tag.
-  $('h1').before($('.stepwise-progress-wrapper'));
+  // Move progress bar to top of page.
+  // If this is an afform, it will have a div#crm-main-content-wrapper; place 
+  //  progressbar just before that element.
+  // Otherwise, look for the first h1 tag and place progressbar just before it.
+  // Note: If a WP page does not contain an h1 tag, the progress bar will not appear.
+  var el;
+  if ($('h1').length) {
+    el = $('h1');
+  }
+  else {
+    el = $('div#crm-main-content-wrapper');
+  }
+  $('.stepwise-progress-wrapper').insertBefore(el);
   $('.stepwise-progress-wrapper').show();
 
 
