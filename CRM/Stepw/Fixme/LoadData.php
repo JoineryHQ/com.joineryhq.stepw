0faf59b979db9edae4fd07273c23d75fc98d8412
Allen Shaw <allen@JoineryHQ.com>
2025-03-06 17:44:15 -0600

    getSampleData(): change location for server-local workflow data config file.

--- CRM/Stepw/Fixme/LoadData.php
+++ CRM/Stepw/Fixme/LoadData.php
@@ -3,7 +3,15 @@ use CRM_Stepw_ExtensionUtil as E;
 
 class CRM_Stepw_Fixme_LoadData {
   public static function getSampleData() {
-    $localDataFile = __DIR__ . '/_localData.php';
+    // Preferred location for server-local workflow data config file is adjacent to civicrm.settings.php
+    $civicrmSettingsDir = dirname(CIVICRM_SETTINGS_PATH ?? '');
+    if (file_exists($civicrmSettingsDir . '/_stepwLocalData.php')) {
+      $localDataFile = $civicrmSettingsDir . '/_stepwLocalData.php';
+    }
+    else {
+      // Alternate (previously the only) location is adjacent to this file.
+      $localDataFile = __DIR__ . '/_localData.php';
+    }
     if (file_exists($localDataFile)) {
       $data = require $localDataFile;
     }
