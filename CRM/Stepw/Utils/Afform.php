fdb7e1b35f294095cba2c762a3b2c56541f3dd32
Allen Shaw <allen@JoineryHQ.com>
2025-07-30 16:08:53 -0500

    Force form-based security ("FBAC") on workflow afforms.

--- CRM/Stepw/Utils/Afform.php
+++ CRM/Stepw/Utils/Afform.php
@@ -11,6 +11,7 @@
  * @author as
  */
 class CRM_Stepw_Utils_Afform {
+
   public static function alterForm(phpQueryObject $doc) {
     // $doc is a phpquery object:
     //  - built with code in: https://github.com/TobiaszCudnik/phpquery)
@@ -19,8 +20,8 @@ class CRM_Stepw_Utils_Afform {
     // fixme: Change any RBAC (role-based) security to FBAC (form-based), because
     //   we want this to work for anon users; we are going to handle
     //   validation/permissions via stepw_* query params.
-    //  $doc->find('af-entity')->attr('security', 'FBAC');
-    
+    $doc->find('af-entity')->attr('security', 'FBAC');
+
     // Force 'url-autofill' to true for Individual1, so that we're sure to support
     // continued tracking of the contact Id (created from the first form submission)
     // in subsequent forms in the workflowInstance.
@@ -29,19 +30,19 @@ class CRM_Stepw_Utils_Afform {
     // Change the button properties and relocate it into a new <div> that uses our controller.
     /* TODO: Here I'm altering the form to achieve  the following:
      *  - Overide the logic that hides the submit button, by replacing the
-     *    button's ng-if attribute with my own. 
+     *    button's ng-if attribute with my own.
      *  - Alter the button label with a value that's passed to CRM.vars in hook_civicrm_pageRun().
-     * 
-     * But i had scoping issues, and the only way I could 
-     * figure to resolve that was to wrap the button in a div that uses my custom 
+     *
+     * But i had scoping issues, and the only way I could
+     * figure to resolve that was to wrap the button in a div that uses my custom
      * controller.
-     * 
+     *
      * Scoping issue were (I think):
      * - my function stepwiseShowSubmitButton() does not exist in current $scope,
      *   and I can't see how to add it thereto.
      * - my angular controller's init() function doesn't seem to be able to modify
      *   variables in current $scope.
-     * 
+     *
      * -- per coleman: Apparently this is a race condition in my controller.init(),
      *    in which $scope.$parent.$scope is not completely ready by the time my init()
      *    fires. So, todo: implement in controller:
@@ -58,8 +59,8 @@ class CRM_Stepw_Utils_Afform {
       $buttonHtml
       {{ hideIfInvalid() }}
       </div>
-    END;   
+    END;
     $doc->find('af-form')->append($appendToDoc);
-    
   }
+
 }
